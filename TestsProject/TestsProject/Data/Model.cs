670272f85d976e4a790d45007972cb60b5bcdd1e
vladimir.zabelin@devexpress.com <vladimir.zabelin@devexpress.com>
2019-07-13 17:02:17 +0300

    Test

---
+++ TestsProject/TestsProject/Data/Model.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestsProject.Data
+{
+    class Categories
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public string Description { get; set; }
+    }
+    class Products
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int CategoryId { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int UnitsInStock { get; set; }
+        public string QuantityPerUnit { get; set; }
+        public bool Discontinued { get; set; }
+    }
+}
---
+++ TestsProject/TestsProject/Data/WebServicePath.cs
@@ -0,0 +1,7 @@
+﻿namespace TestsProject
+{
+    internal class WebServicePath
+    {
+        public string Path { get { return "https://localhost:44378/api/"; } }
+    }
+}
